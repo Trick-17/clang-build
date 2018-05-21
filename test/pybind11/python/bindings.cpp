243930fda298fdbefbd8e164f7c4eb8a275b9e8e
Gideon <g.mueller@fz-juelich.de>
2018-05-21 22:31:56 +0200

    Added files for a pzbind11 build test.

---
+++ test/pybind11/python/bindings.cpp
@@ -0,0 +1,20 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+#include <pybind11/stl.h>
+
+#include <mylib.hpp>
+
+#include <memory>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(mylib, m)
+{
+    m.attr("__version__") = "dev";
+
+    // Module docstring
+    m.doc() = "This is my library...";
+
+    // Module functions
+    m.def("calculate", &mylib::triple, "calculate the triple of a number");
+}
---
+++ test/pybind11/src/mylib.cpp
@@ -0,0 +1,9 @@
+#include <mylib.hpp>
+
+namespace mylib
+{
+    float triple(float x)
+    {
+        return 3*x;
+    }
+}
