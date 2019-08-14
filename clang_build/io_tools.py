c9f32938e1eaf11189bd2eddb125e3b7a5134454
Nicholas Ohs <nicholas.ohs@outlook.com>
2019-08-14 23:50:03 +0200

    Simplified more code in io_tools.

--- clang_build/io_tools.py
+++ clang_build/io_tools.py
@@ -28,22 +28,12 @@ def get_sources_and_headers(target_options, target_root_directory, target_build_
     include_options = []
     include_options += target_options.get('include_directories', [])
 
-    if 'osx' in target_options and _platform.PLATFORM == 'osx':
-        include_options += target_options['osx'].get('include_directories', [])
-    if 'windows' in target_options and _platform.PLATFORM == 'windows':
-        include_options += target_options['windows'].get('include_directories', [])
-    if 'linux' in target_options and _platform.PLATFORM == 'linux':
-        include_options += target_options['linux'].get('include_directories', [])
+    include_options += target_options.get(_platform.PLATFORM, {}).get('include_directories', [])
 
     exclude_options = []
     exclude_options += target_options.get('headers_exclude', [])
 
-    if 'osx' in target_options and _platform.PLATFORM == 'osx':
-        exclude_options += target_options['osx'].get('headers_exclude', [])
-    if 'windows' in target_options and _platform.PLATFORM == 'windows':
-        exclude_options += target_options['windows'].get('headers_exclude', [])
-    if 'linux' in target_options and _platform.PLATFORM == 'linux':
-        exclude_options += target_options['linux'].get('headers_exclude', [])
+    exclude_options += target_options.get(_platform.PLATFORM, {}).get('headers_exclude', [])
 
     include_patterns = list(set([target_root_directory.joinpath(path) for path in include_options]))
     exclude_patterns = list(set([target_root_directory.joinpath(path) for path in exclude_options]))
@@ -56,29 +46,13 @@ def get_sources_and_headers(target_options, target_root_directory, target_build_
         output['include_directories'] += [target_root_directory.joinpath(''), target_root_directory.joinpath('include'), target_root_directory.joinpath('thirdparty')]
         output['headers'] += _get_header_files_in_folders(output['include_directories'], exclude_patterns=exclude_patterns, recursive=False)
 
-    # Options for public include directories
+    # Options for public include directories, exclude patterns are the same
     include_options_public = []
     include_options_public += target_options.get('include_directories_public', [])
 
-    if 'osx' in target_options and _platform.PLATFORM == 'osx':
-        include_options_public += target_options['osx'].get('include_directories_public', [])
-    if 'windows' in target_options and _platform.PLATFORM == 'windows':
-        include_options_public += target_options['windows'].get('include_directories_public', [])
-    if 'linux' in target_options and _platform.PLATFORM == 'linux':
-        include_options_public += target_options['linux'].get('include_directories_public', [])
-
-    exclude_options = []
-    exclude_options += target_options.get('headers_exclude', [])
-
-    if 'osx' in target_options and _platform.PLATFORM == 'osx':
-        exclude_options += target_options['osx'].get('headers_exclude', [])
-    if 'windows' in target_options and _platform.PLATFORM == 'windows':
-        exclude_options += target_options['windows'].get('headers_exclude', [])
-    if 'linux' in target_options and _platform.PLATFORM == 'linux':
-        exclude_options += target_options['linux'].get('headers_exclude', [])
+    include_options_public += target_options.get(_platform.PLATFORM, {}).get('include_directories_public', [])
 
     include_patterns = list(set([target_root_directory.joinpath(path) for path in include_options_public]))
-    exclude_patterns = list(set([target_root_directory.joinpath(path) for path in exclude_options]))
 
     # Find header files
     if include_patterns:
@@ -92,23 +66,13 @@ def get_sources_and_headers(target_options, target_root_directory, target_build_
     sources_options = []
     sources_options += target_options.get('sources', [])
 
-    if 'osx' in target_options and _platform.PLATFORM == 'osx':
-        sources_options += target_options['osx'].get('sources', [])
-    if 'windows' in target_options and _platform.PLATFORM == 'windows':
-        sources_options += target_options['windows'].get('sources', [])
-    if 'linux' in target_options and _platform.PLATFORM == 'linux':
-        sources_options += target_options['linux'].get('sources', [])
+    sources_options += target_options.get(_platform.PLATFORM, {}).get('sources', [])
 
     exclude_options = []
     exclude_options += target_options.get('sources_exclude', [])
 
-    if 'osx' in target_options and _platform.PLATFORM == 'osx':
-        exclude_options += target_options['osx'].get('sources_exclude', [])
-    if 'windows' in target_options and _platform.PLATFORM == 'windows':
-        exclude_options += target_options['windows'].get('sources_exclude', [])
-    if 'linux' in target_options and _platform.PLATFORM == 'linux':
-        exclude_options += target_options['linux'].get('sources_exclude', [])
-
+    exclude_options += target_options.get(_platform.PLATFORM, {}).get('sources_exclude', [])
+    
     sources_patterns = list(set([target_root_directory.joinpath(path) for path in sources_options]))
     exclude_patterns = list(set([target_root_directory.joinpath(path) for path in exclude_options]))
 
